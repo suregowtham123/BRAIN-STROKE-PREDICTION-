c8f4a33675c2dd3c105216acfd107768c9c8906f
suregowtham123 <132137128+suregowtham123@users.noreply.github.com>
2025-08-04 15:52:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,6 @@ def predict():
         return render_template('index.html', error="Error making prediction. Check input values.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
+
+
